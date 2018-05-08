6b2e02a6ae2d763b1e73938a1225d3f8e52a776d
John Hampton <john_j_hampton@yahoo.com>
2018-05-08 15:12:33 -0400

    Lift up date state from Datepicker to App component

--- src/App.js
+++ src/App.js
@@ -5,15 +5,32 @@ import './App.css';
 import Datepicker from './Datepicker';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      date: new Date()
+    };
+  }
+
+  dateChanged(date) {
+    console.log('date changed to', date);
+  }
+
   render() {
+    const { date } = this.state;
+    
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Doomsday email generator!</h1>
-          <h2>We will help you compose an email to your boss about humanity's impending doom ... and why you won't make it to work</h2>
+          <h2>
+            We will help you compose an email to your boss about humanity's
+            impending doom ... and why you won't make it to work
+          </h2>
         </header>
-        <Datepicker></Datepicker>
+        <Datepicker date={date} onDateChange={this.dateChanged}/>
       </div>
     );
   }
--- src/Datepicker.js
+++ src/Datepicker.js
@@ -1,35 +1,17 @@
 import "flatpickr/dist/themes/material_green.css";
 
 import Flatpickr from "react-flatpickr";
-import React, { Component } from "react";
+import React from "react";
 
-class Datepicker extends Component {
-  constructor() {
-    super();
+const Datepicker = props => {
+    const { date, onDateChange } = props;
 
-    this.state = {
-      date: new Date()
-    };
-  }
-
-  handleChange(date) {
-    this.setState({ date });
-  }
-
-  render() {
-    const { date } = this.state;
     return (
       <div>
         <p>Enter the date that you'll be unavailable for work.</p>
-        <Flatpickr
-          value={date}
-          onChange={date => {
-            this.handleChange(date)
-          }}
-        />
+        <Flatpickr value={date} onChange={date => onDateChange(date)} />
       </div>
     );
-  }
 }
 
 export default Datepicker;
